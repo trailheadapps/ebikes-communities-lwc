c0467410823f0acfcd46588a5846d96dbe4b1824
Kenan Mesic <kenmesi@yahoo.com>
2019-04-23 13:24:04 -0700

    Created E-Bikes on Salesforce Communities

--- force-app/main/default/lwc/pubsub/pubsub.js
+++ force-app/main/default/lwc/pubsub/pubsub.js
@@ -6,16 +6,6 @@
 
 const events = {};
 
-const samePageRef = (pageRef1, pageRef2) => {
-    const obj1 = pageRef1.attributes;
-    const obj2 = pageRef2.attributes;
-    return Object.keys(obj1)
-        .concat(Object.keys(obj2))
-        .every(key => {
-            return obj1[key] === obj2[key];
-        });
-};
-
 /**
  * Registers a callback for an event
  * @param {string} eventName - Name of the event to listen for.
@@ -23,19 +13,14 @@ const samePageRef = (pageRef1, pageRef2) => {
  * @param {object} thisArg - The value to be passed as the this parameter to the callback function is bound.
  */
 const registerListener = (eventName, callback, thisArg) => {
-    // Checking that the listener has a pageRef property. We rely on that property for filtering purpose in fireEvent()
-    if (!thisArg.pageRef) {
-        throw new Error(
-            'pubsub listeners need a "@wire(CurrentPageReference) pageRef" property'
-        );
-    }
-
     if (!events[eventName]) {
         events[eventName] = [];
     }
+
     const duplicate = events[eventName].find(listener => {
         return listener.callback === callback && listener.thisArg === thisArg;
     });
+
     if (!duplicate) {
         events[eventName].push({ callback, thisArg });
     }
@@ -78,12 +63,10 @@ const fireEvent = (pageRef, eventName, payload) => {
     if (events[eventName]) {
         const listeners = events[eventName];
         listeners.forEach(listener => {
-            if (samePageRef(pageRef, listener.thisArg.pageRef)) {
-                try {
-                    listener.callback.call(listener.thisArg, payload);
-                } catch (error) {
-                    // fail silently
-                }
+            try {
+                listener.callback.call(listener.thisArg, payload);
+            } catch (error) {
+                // fail silently
             }
         });
     }
