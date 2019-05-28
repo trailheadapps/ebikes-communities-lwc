ca9a3f889afb8ace8e3b7c66d4c4e1800d89849e
Kenan <kenan.mesic@salesforce.com>
2019-05-28 16:57:13 -0700

    Added lcc component (3d model of bike)

---
+++ force-app/main/default/aura/lccButtonsViewBikeParts/lccButtonsViewBikePartsController.js
@@ -0,0 +1,7 @@
+({  
+    viewBikePart : function (component, event, helper) {
+        // Get the bike part of the whatever component called this function
+        var bikePart = event.getSource().get('v.title').replace('View', '');
+        helper.sendMsgToLCC({ "bikePart": bikePart });
+    }
+})
